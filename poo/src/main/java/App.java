bc24bb74f964ca379871c6a6bb207ca281e34721
Nat Fioren <62964759+nafioren@users.noreply.github.com>
2022-05-11 02:01:35 -0300

    Add files via upload

--- poo/src/main/java/App.java
+++ poo/src/main/java/App.java
@@ -1,23 +1,23 @@
-
-
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
-        Person p1 = new Person();
-        System.out.println("El nombre y apellido de la person 1 es: " + p1.getName() + " " + p1.getLastName());
-        System.out.println("Ingrese el nombre de la person 2: ");
-        Person p2 = new Person(read.next());
-        System.out.println("El nombre y apellido de la person 2 es: " + p2.getName() + " " + p2.getLastName());
-        System.out.println("Ingrese el nombre de la person 3: ");
-        String name = read.next();
-        System.out.println("Ingrese el apellido de la person 3: ");
-        String lastName = read.next();
-        Person p3 = new Person(name, lastName);
-        System.out.println("El nombre y apellido de la person 3 es: " + p3.getName() + " " + p3.getLastName());
 
+        Result resultados = new Result();
+        Arithmetic2 cuentas = new Arithmetic2();
+
+        double num1 = 27.4;
+        double num2 = 22.9;
+
+
+        cuentas.sum(resultados, num1, num2);
+        System.out.println("Suma = " + resultados.getResultSum());
+
+        cuentas.subtraction(resultados, num1, num2);
+        System.out.println("Resta = " + resultados.getResultSubstraction());
+
+        cuentas.multiplication(resultados, num1, num2);
+        System.out.println("Multiplicación = " + resultados.getMultiplication());
+
+        cuentas.division(resultados, num1, num2);
+        System.out.println("División = " + resultados.getDivision());
     }
-
-
 }
---
+++ poo/src/main/java/Result.java
@@ -0,0 +1,42 @@
+
+public class Result {
+    private double resultSum;
+    private double resultSubstraction;
+    private double multiplication;
+    private double division;
+
+    public Result() {
+    }
+
+    public double getResultSum() {
+        return resultSum;
+    }
+
+    public void setResultSum(double resultSum) {
+        this.resultSum = resultSum;
+    }
+
+    public double getResultSubstraction() {
+        return resultSubstraction;
+    }
+
+    public void setResultSubstraction(double resultSubstraction) {
+        this.resultSubstraction = resultSubstraction;
+    }
+
+    public double getMultiplication() {
+        return multiplication;
+    }
+
+    public void setMultiplication(double multiplication) {
+        this.multiplication = multiplication;
+    }
+
+    public double getDivision() {
+        return division;
+    }
+
+    public void setDivision(double division) {
+        this.division = division;
+    }
+}
